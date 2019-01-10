ce190c1355376d23fc846be24226818e129d16fe
Yuichi Ishiyama <yufroms@gmail.com>
2019-01-11 07:01:01 +0900

    made welcome page

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <p class="logo">FromScratch</p>
+    <p class="logo"><nuxt-link to="/"><img class="logo" src="~assets/img/logo.png" alt="Yuichi Ishiyama's logo"></nuxt-link></p>
     <p class="menu button">Menu</p>
   </header>
 </template>
--- components/Nav.vue
+++ components/Nav.vue
@@ -2,9 +2,6 @@
   <nav>
     <ul>
       <li>
-        <nuxt-link to="/">Top</nuxt-link>
-      </li>
-      <li>
         <nuxt-link to="/about">About</nuxt-link>
       </li>
       <li>
