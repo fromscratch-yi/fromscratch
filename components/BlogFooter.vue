9343fb527c2bde1ac8018a1a6fea2cd2a6a64923
Yuichi Ishiyama <yufroms@gmail.com>
2021-03-15 13:17:09 +0900

    fix

--- components/BlogFooter.vue
+++ components/BlogFooter.vue
@@ -86,7 +86,7 @@
         </div>
       </section>
     </div>
-    <p class="copyright">&copy;2020 FromScratch All Rights Reserved</p>
+    <p class="copyright">&copy;2021 FromScratch All Rights Reserved</p>
   </footer>
 </template>
 <script>
