2c798832bec374b94a73f5b69cd20b6557535683
Yuichi Ishiyama <yufroms@gmail.com>
2022-05-13 01:22:51 +0900

    add swift sintax

--- plugins/prism.ts
+++ plugins/prism.ts
@@ -4,5 +4,6 @@ import Prism from 'prismjs';
 import 'prismjs/themes/prism-okaidia.css';
 // 使いたい言語のシンタックスを設定
 import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-swift';
 
 export default Prism;
